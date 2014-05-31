60e6842193c80705bb42b43692ccfb8991b89521
tabul12 <tabul12@freeuni.edu.ge>
2014-05-31 17:07:22 +0400

    start our project

--- EventListener/src/User.java
+++ EventListener/src/User.java
@@ -1,17 +1,4 @@
 
 public class User {
-	public static void main(String[] args) {
-		System.out.println("start");
-		System.out.println("first change");
-		System.out.println("Second change");
-		System.out.print("blabla");
-		System.out.print("third");
-		System.out.println("makmdada");
-		System.out.println("cloo");
-		// yuyyyy
-		// me
-		// imushave
-		// hjdhdhhhhhhhhhhhhhhhhhhhh
-		// vchamot....
-	}
+	
 }
