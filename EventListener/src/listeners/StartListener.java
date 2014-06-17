01160c86bb05d3a22cb8e4279f2417ca57bb36eb
tabul12 <tabul12@freeuni.edu.ge>
2014-06-17 16:36:25 +0400

    davamate sesion listeneri

---
+++ EventListener/src/listeners/StartListener.java
@@ -0,0 +1,62 @@
+package listeners;
+
+import java.sql.SQLException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
+
+import baseConnection.BandManager;
+import baseConnection.EventConnectionPool;
+import baseConnection.EventManager;
+import baseConnection.PlaceManager;
+import baseConnection.UserManager;
+
+/**
+ * Application Lifecycle Listener implementation class StartListener
+ *
+ */
+@WebListener
+public class StartListener implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public StartListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent arg0) {
+    	try {
+			EventConnectionPool evConPool = new EventConnectionPool();
+			BasicDataSource dataSource = evConPool.getEventDataSource();
+			UserManager uManager = new UserManager(dataSource);
+			BandManager bManager = new BandManager(dataSource);
+			PlaceManager pManager =  new PlaceManager(dataSource);
+			EventManager eManager = new EventManager(dataSource);
+			ServletContext context = arg0.getServletContext();
+			context.setAttribute("UserManager", uManager);
+			context.setAttribute("BandManager", bManager);
+			context.setAttribute("PlaceManager", pManager);
+			context.setAttribute("EventManager", eManager);
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent arg0) {
+        // TODO Auto-generated method stub
+    }
+	
+}
