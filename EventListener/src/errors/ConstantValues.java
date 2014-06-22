2de3111fdb2e820e917cd0165314ed58ee57e9ff
maghriany <mmagh12@freeuni.edu.ge>
2014-06-22 16:14:36 +0400

    userpage

---
+++ EventListener/src/errors/ConstantValues.java
@@ -0,0 +1,22 @@
+package errors;
+
+public class ConstantValues {
+
+	public static final int NUM_EVENT_ON_PER_PAGE = 8;
+	public static final int NUM_LEFT_RIGHT_PAGES = 5;
+	public static final int NUM_TOP_BANDS = 10;
+	public static final int NUM_TOP_PLACES = 10;
+	public static final int NUM_MY_EVENT_ON_PER_PAGE = 4;
+	public static final int NUM_MY_BAND_ON_PER_PAGE = 4;
+	public static final int NUM_MY_PLACE_ON_PER_PAGE = 4;
+	public static final int NUM_WISHLIST_ON_PER_PAGE = 4;
+	public static final int NUM_BEEN_PLACE_ON_PER_PAGE = 4;
+	public static final int NUM_ROWS_OF_GOING_USERS = 2;
+	public static final int NUM_USERS_IN_PER_ROW = 5;
+	public static final int NUM_GOING_USERS_PER_PAGE = NUM_ROWS_OF_GOING_USERS * NUM_USERS_IN_PER_ROW;
+	public static final int PLACE_IMAGES_ON_PER_PAGE = 10;
+	public static final int MAX_SCORE = 5;
+	public static final int NUMBER_OF_VIDEOS_PER_PAGE_FOR_BAND_PROFILE = 1; 
+	public static final int NUMBER_OF_IMAGES_PER_PAGE_FOR_BAND_PROFILE = 1;
+	public static final int NUMBER_OF_MUSICS_PER_PAGE_FOR_BAND_PROFILE = 1;
+}
